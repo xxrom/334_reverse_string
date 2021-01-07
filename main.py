8da45bdf748bf7b5e73ff1b57816e036d1ebc9aa
nchernyshov <xpom55@mail.ru>
2021-01-07 23:30:16 +0300

    solved: 264ms (5%), 18.4mb (92.5%) easy one solution

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from typing import List
+
+
+class Solution:
+
+  def reverseString(self, s: List[str]) -> None:
+    size = len(s) - 1
+    middle = size // 2
+
+    for i in range(middle):
+      s[i], s[size - i] = s[size - i], s[i]
+
+    return s
+
+
 my = Solution()
-n = 0
-trueAns=0
-ans = my.X(n)
+n = ["h", "e", "l", "l", "o"]
+trueAns = ["o", "l", "l", "e", "h"]
+
+ans = my.reverseString(n)
+
 print("ans", ans, ans == trueAns)
